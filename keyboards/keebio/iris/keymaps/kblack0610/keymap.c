92deb6e0eccefd7bb6ee2471c53876db6e89462f
Kenneth <kblack0610@gmail.com>
2025-12-30 16:10:19 -0800

    update foostan fixes

--- keyboards/keebio/iris/keymaps/kblack0610/keymap.c
+++ keyboards/keebio/iris/keymaps/kblack0610/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      ADJUST, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, MT(MOD_LCTL, KC_Z), KC_X,    KC_C,    KC_V,    KC_B,    KC_HOME,          KC_ESC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SCLN,
+     KC_LSFT, MT(MOD_LCTL, KC_Z), MT(MOD_LALT, KC_X),    KC_C,    KC_V,    KC_B,    KC_HOME,          KC_ESC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SCLN,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     LOWER,  KC_LGUI,   MT(MOD_LCTL, KC_TAB),     KC_ENT,  KC_SPC, RAISE
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
